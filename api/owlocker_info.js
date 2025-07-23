58a3e7e02cd94fd938556f9858faff07b7cc4ffe
B1129005 <113523539+zchsu@users.noreply.github.com>
2025-07-23 15:42:36 +0800

    locker_api_cors

--- api/owlocker_info.js
+++ api/owlocker_info.js
@@ -1,6 +1,14 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // 允許所有來源
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   try {
     const response = await axios.get('https://owlocker.com/api/info', { timeout: 10000 });
     res.status(200).json(response.data);
